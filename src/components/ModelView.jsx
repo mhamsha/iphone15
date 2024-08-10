ab4fba0a5ede2020141c2e7e88243eddef1aced1
mhamsha <hamza.shahzad.8692@gmail.com>
2024-08-10 16:20:01 +0500

    chore: Remove unused code for lazy loading images in ModelView.jsx

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -3,80 +3,51 @@ import * as THREE from "three";
 import Lights from "./Lights";
 import Loader from "./Loader";
 import IPhone from "./Iphone";
-import { useState, useEffect, Suspense, useRef } from "react";
+import { useState, Suspense } from "react";
 
 const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationState, size, item }) => {
   const [isGrabbing, setIsGrabbing] = useState(false);
-  // const [shouldLoad, setShouldLoad] = useState(false);
-
-  // const wrapperRef = useRef(null);
-
-  // useEffect(() => {
-  //   const observer = new IntersectionObserver(
-  //     ([entry]) => {
-  //       if (entry.isIntersecting) {
-  //         setShouldLoad(true);
-  //         observer.disconnect();
-  //       }
-  //     },
-  //     {
-  //       root: null,
-  //       rootMargin: "0px",
-  //       threshold: 0.1,
-  //     }
-  //   );
-
-  //   if (wrapperRef.current) {
-  //     observer.observe(wrapperRef.current);
-  //   }
-
-  //   return () => {
-  //     if (wrapperRef.current) {
-  //       observer.unobserve(wrapperRef.current);
-  //     }
-  //   };
-  // }, []);
   return (
-        <View
-          index={index}
-          id={gsapType}
-          // className={`w-full h-full absolute ${index === 2 ? "right-[-100%]" : ""}`}
-          className={`w-full h-full absolute ${index === 2 ? "right-[-100%]" : ""} ${isGrabbing ? "cursor-grabbing" : "cursor-grab"} overflow-hidden`}
-        >
-          {/* Ambient Light */}
-          <ambientLight intensity={0.3} />
+    <View
+      index={index}
+      id={gsapType}
+      // className={`w-full h-full absolute ${index === 2 ? "right-[-100%]" : ""}`}
+      className={`w-full h-full absolute ${index === 2 ? "right-[-100%]" : ""} ${isGrabbing ? "cursor-grabbing" : "cursor-grab"} overflow-hidden`}
+    >
+      {/* Ambient Light */}
+      <ambientLight intensity={0.3} />
 
-          <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+      <PerspectiveCamera makeDefault position={[0, 0, 4]} />
 
-          <Lights />
+      <Lights />
 
-          <OrbitControls
-            makeDefault
-            ref={controlRef}
-            enableZoom={false}
-            enablePan={false}
-            rotateSpeed={0.4}
-            target={new THREE.Vector3(0, 0, 0)}
-            // maxPolarAngle={Math.PI / 2} // Set both maxPolarAngle and minPolarAngle to the same value
-            // minPolarAngle={Math.PI / 2} // This will disable vertical rotation
-            // onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
-            onStart={() => setIsGrabbing(true)}
-            onEnd={() => {
-              setIsGrabbing(false);
-              setRotationState(controlRef.current.getAzimuthalAngle());
-            }}
+      <OrbitControls
+        makeDefault
+        ref={controlRef}
+        enableZoom={false}
+        enablePan={false}
+        rotateSpeed={0.4}
+        target={new THREE.Vector3(0, 0, 0)}
+        // maxPolarAngle={Math.PI / 2} // Set both maxPolarAngle and minPolarAngle to the same value
+        // minPolarAngle={Math.PI / 2} // This will disable vertical rotation
+        // onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
+        onStart={() => setIsGrabbing(true)}
+        onEnd={() => {
+          setIsGrabbing(false);
+          setRotationState(controlRef.current.getAzimuthalAngle());
+        }}
+      />
+      <group ref={groupRef} name={`${index === 1 ? "small" : "large"}`} position={[0, 0, 0]}>
+        <Suspense fallback={<Loader />}>
+          <IPhone
+            key={`${item.color[0]}-${size}`}
+            scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
+            item={item}
+            size={size}
           />
-          <group ref={groupRef} name={`${index === 1 ? "small" : "large"}`} position={[0, 0, 0]}>
-            <Suspense fallback={<Loader />}>
-              <IPhone
-                key={size}
-                scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
-                item={item}
-                size={size}
-              />
-            </Suspense>
-          </group>
-        </View>
+        </Suspense>
+      </group>
+    </View>
   );
 };
 
